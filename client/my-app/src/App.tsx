d35a7afacc4d1746efbb589ebbab52412730ed64
Alexandra <donchenkoalexandra@gmail.com>
2021-01-13 12:51:25 +0100

    setted up enviroment for typescript

---
+++ client/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import React from 'react';
+const hi: string = 'Hello';
+function App() {
+  return (
+    <div className="App">
+      <span> {hi}</span>
+    </div>
+  );
+}
+
+export default App;
