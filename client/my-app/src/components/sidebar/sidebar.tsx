daf2222818d25a94a4d4cf8adcaa895c2800ad1d
Alexandra <donchenkoalexandra@gmail.com>
2021-01-15 16:24:58 +0100

    side bar component has a button that will display form and for is able to create new Diaries and add them to sidebar

--- client/my-app/src/components/sidebar/sidebar.tsx
+++ client/my-app/src/components/sidebar/sidebar.tsx
@@ -1,34 +1,59 @@
 import { useState } from 'react';
-import Modal from './../modal./modal';
+//import Modal from './../modal./modal';
+interface Diary {
+  diaryName: string;
+  entries:
+    | [
+        entrie: {
+          date: Date;
+          text: string;
+        }
+      ]
+    | [];
+}
+
 function Sidebar() {
   const [modal, setModal] = useState(false);
-  function printMe(e: any) {
-    e.preventDefault();
-    console.log('buttonpressed');
-  }
+  const [diaries, setDiaries] = useState<Diary[]>([]);
+  const [justName, setJustName] = useState('');
+  const [diaryName, setDiaryName] = useState<Diary>();
+  //const [toSubmit, setSubmit] = useState(false);
   const toggle = function () {
     setModal(!modal);
   };
-  const createDiary = () => {};
+  function handleName(e: any) {
+    setJustName(e.target.value);
+    setDiaryName({ diaryName: justName, entries: [] });
+  }
+  function createDiary(e: React.FormEvent) {
+    e.preventDefault();
+    const oldDiaries = diaries;
+    if (diaryName !== undefined) {
+      setDiaries([...oldDiaries, diaryName]);
+    }
+  }
+  console.log(diaries);
   return (
     <div>
       <button onClick={toggle}>CREATE NEW DIARY</button>
+
+      {console.log(modal)}
       {modal ? (
-        <Modal>
+        <div>
           <div>
             <span>CHOOOSE NAME FOR YOUR DIARY</span>
-            <form onSubmit={printMe}>
+            <form onSubmit={createDiary}>
               <label>
                 NAME YOUR DIARY
-                <input type="text" />
+                <input type="text" value={justName} onChange={handleName} />
               </label>
-              <button type="submit" value="Submit" onChange={printMe}>
+              <button type="submit" value="Submit" onSubmit={createDiary}>
                 Create new Diary
               </button>
               <button onClick={toggle}> CLOSE</button>
             </form>
           </div>
-        </Modal>
+        </div>
       ) : null}
     </div>
   );
