a2e6d057bbf96d3520e90994ab2e0ecff9f82789
Alexandra <donchenkoalexandra@gmail.com>
2021-02-13 16:54:06 +0100

    dashboard component refactored and styled

--- client/my-app/src/components/dashboard/dashboard.tsx
+++ client/my-app/src/components/dashboard/dashboard.tsx
@@ -1,27 +1,30 @@
-import './dashboard.module.css';
-import { navigate } from '@reach/router';
+import styles from './Dashboard.module.css';
+import { navigate, Link} from '@reach/router';
 import Header from '../header/header';
+import picture from './../../resources/undraw_Personal_notebook_re_d7dc.svg'
 function DashBoard(props: object) {
   console.log(props);
   function handleClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     navigate('/sign-up');
   }
   return (
-    <div className="dashboard">
+    <div className={styles.dashboard}>
       <Header />
-      <div className="welcome-container">
-        <div className="welcome">WELCOME </div>
-        <div className="dash-component">
-          <div className="description">
+      <div className={styles.welcomecontainer}>
+        <div className={styles.welcome}>WELCOME </div>
+        <div className={styles.dashcomponent}>
+          <img src={picture} alt='diary'></img>
+          <div className={styles.description}>
             Mood is diary app that helps you track your mood by using sentiment
             analysis.
           </div>
-          <div className="butto-div">
-            <button className="redirect" onClick={handleClick}>
-              Sign up
-            </button>
-          </div>
         </div>
+        <div className={styles.redirect}>
+          <Link className={styles.redirect} to='/sign-up'>
+            Sign up
+          </Link>
+        </div>
+        
       </div>
     </div>
   );
